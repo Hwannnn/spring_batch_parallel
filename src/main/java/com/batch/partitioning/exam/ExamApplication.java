ce46133668db89a7343d82b746c20b6a499b69bd
신창환 <changhwan-sin@nhn.com>
2018-10-27 20:34:54 +0900

    init
    - 초기설정

---
+++ src/main/java/com/batch/partitioning/exam/ExamApplication.java
@@ -0,0 +1,13 @@
+package com.batch.partitioning.exam;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableBatchProcessing
+public class ExamApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ExamApplication.class, args);
+	}
+}
